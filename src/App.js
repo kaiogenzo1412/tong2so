ceecc9e5783fc911cd7700a7da1e2b7d68af90b7
Huy1004 <kaiogenzo1412@gmail.com>
2023-04-08 15:58:37 +0700

    fish

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 // rfc
-import React,{}
+import React, { useState } from 'react';
 export default function App() {
-  const
-// sử dụng useState
-  return (
-      <>
-          <div className="container">
-              <div className="form-group">
-                  Số A: <input type="number" className="form-control" />
-              </div>
+    // Sử dụng useState 
+    const [sum, setSum] = useState(0);
+    const [soA, setSoA] = useState(0);
+    const [soB, setSoB] = useState(0);
+    const handleSum = () => {
+        setSum (Number (soA) +Number (soB));
+    }
+    return (
+        <>
+            <div className="container">
+                <div className="form-group">
+                    Số A: <input type="number" className="form-control" onChange={(e) => setSoA(e.target.value)} />
+                </div>
 
-              <div className="form-group">
-                  Số B: <input type="number" className="form-control" />
-              </div>
-              <div className="form-group">
-
-
-
-              </div>
-              <div className="form-group">
-                  <button className="btn btn-primary">Tính</button>
-              </div>
-          </div>
-      </>
-  );
+                <div className="form-group">
+                    Số B: <input type="number" className="form-control" onChange={(e) => setSoB(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    Tổng: {sum}
+                </div>
+                <div className="form-group">
+                    <button className="btn btn-primary" onClick={handleSum}>Tính</button>
+                </div>
+            </div>
+        </>
+    );
 }
